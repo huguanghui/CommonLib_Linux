de9bbde8b1e75d97cf89da6824768e7e4854c24d
huguanghui <522146829@qq.com>
2019-03-27 08:43:49 +0800

    添加demo

--- easydemo/demo.c
+++ easydemo/demo.c
@@ -1,11 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdarg.h>
+
+//仅仅是打印函数名字替换 DEBUG <--> printf
+#define DEBUG(format, ...) printf(format, ##__VA_ARGS__)
+
+//替换打印函数，在打印出来的内容加上前缀
+#define XFUNC_PRINT(format, arg...) printf("XFUNC: " format "", ##arg)
+
+//名字替换，并在打印出来的内容加上前缀,同时加入定位的功能。
+#define TRC_P(fmt, args...) fprintf(stderr,"  TRC_P(%s:%d):\t" fmt, __func__, __LINE__, ##args)
+
+//名字替换，并在打印出来的内容加上前缀,同时加入定位的功能，并让打印的前缀具备特殊颜色(\033[1;32m  \033[0m这些表示颜色，\t一定程度上使屏幕输出对齐)
+#define TRC_PG(fmt, args...) fprintf(stderr, "\033[1;32m  TRC_PG(%s:%d):\033[0m" fmt, __func__, __LINE__, ## args)
+
+//名字替换，并在打印出来的内容加上前缀,同时加入定位的功能，并让打印的前缀具备特殊颜色
+#define TRC_PR(fmt, args...) fprintf(stderr, "\033[1;31m  TRC_PR(%s:%d):\t\033[0m" fmt, __func__, __LINE__, ## args)
+
+#define HGH_DBG(fmt, args...) printf("\033[40;33m HGH_DBG(%s %s %d):\t\033[0m"fmt, __FILE__, __func__, __LINE__, ## args)
+
+// 部分颜色代码:
+// 字背景颜色: 40--49                字颜色: 30--39
+// 40: 黑                           30: 黑
+// 41: 红                           31: 红
+// 42: 绿                           32: 绿
+// 43: 黄                           33: 黄
+// 44: 蓝                           34: 蓝
+// 45: 紫                           35: 紫
+// 46: 深绿                         36: 深绿
+// 47：白色                         37：白色
 
 int main(int argc, char *argv[])
 {
     int pagesize = getpagesize();
     printf("PageSize: %d\n", pagesize);
+
+    // DEBUG("abc\n");
+    // XFUNC_PRINT("abc\n");
+    // TRC_P("abc\n");
+    // TRC_PG("abc\n");
+    // TRC_PR("abc\n");
+    HGH_DBG("abc\n");
 
     return 0;
 }
