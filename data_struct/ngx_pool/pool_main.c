8292154476cc5fbdcc00e73bd3753d0a96ffad8b
huguanghui <522146829@qq.com>
2019-04-01 23:42:48 +0800

    refresh pool

--- data_struct/ngx_pool/pool_main.c
+++ data_struct/ngx_pool/pool_main.c
@@ -3,14 +3,15 @@
 
 #include "ngx_pool.h"
 
-//#define HGH_DBG(fmt, args...) printf("\033[40;33m [POOL]-(%s %d):\t\033[0m"fmt,  __func__, __LINE__, ## args)
-
 int main(int argc, char const *argv[])
 {
     ngx_pool_t *pool = NULL;
     // 创建内存池
     pool = ngx_create_pool(512);
     // 重置内存池
+    //ngx_reset_pool(pool);
+    char *buf = ngx_palloc(pool, 4096);
+    snprintf(buf, 4096, "hello world!");
     // 销毁内存池
     ngx_destroy_pool(pool);
     pool = NULL;
