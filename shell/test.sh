0adf920e332ff15198180c8cae8d0a7057ae2ca4
huguanghui <522146829@qq.com>
2019-04-18 18:26:55 +0800

    refresh

--- shell/test.sh
+++ shell/test.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
-for initscript in ./S[0-9][0-9]*
-do
-        if [ -x $initscript ] ;
-        then
-                echo "[RCS]: $initscript"
-                $initscript
-        fi
+# for initscript in ./S[0-9][0-9]*
+# do
+#         if [ -x $initscript ] ;
+#         then
+#                 echo "[RCS]: $initscript"
+#                 $initscript
+#         fi
+# done
+
+#set -x
+
+# i=1.0.0.1
+
+# echo -n > test.conf
+
+# if [ "$i" != "1.0.0.0" ];then
+#     echo nameserver $i >> test.conf
+# fi
+
+#array_name=(value0 value1 value2 value3)
+
+str=["1.0.0.0" "1.0.0.1" "1.0.1.0" "1.1.0.0"]
+
+echo $str
+
+for i in $str ; do
+    echo $i
 done
