ca26fb422dffe79b6b3fa4fa4014d75d7211c3ed
huguanghui <522146829@qq.com>
2019-05-16 21:04:18 +0800

    refresh

--- use_gprof/demo01/test_gprof.c
+++ use_gprof/demo01/test_gprof.c
@@ -1,8 +1,16 @@
 //test_gprof.c
 #include<stdio.h>
  
-void new_func1(void);
+void new_func1(void)
+{
+    printf("\n Inside new_func1()\n");
+    int i = 0;
  
+    for(;i<0xffffffee;i++);
+ 
+    return;
+}
+
 void func1(void)
 {
     printf("\n Inside func1 \n");
