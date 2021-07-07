345a651889fa0685addaadf34fad65ee1f44b621
huguanghui <522146829@qq.com>
2021-07-08 07:37:11 +0800

    feat(test_rc) 接口上传

--- test_bitrate/src/bitrate_calc.c
+++ test_bitrate/src/bitrate_calc.c
@@ -2,5 +2,26 @@
 
 #include "bitrate_calc.h"
 
+#define MAX_FRAME_HIST (120)
 
+struct stream_rc_statistics {
+    BitrateItem fram_hist[MAX_FRAME_HIST];
+    int hist_in_idx;
+    int hist_out_idx;
+    int total_win_size;
+    int framerate;
+}StreamRcStatistics;
+
+void rc_check(int ch)
+{
+    (void)ch;
+    return;
+}
+
+void rc_filled(int ch, const BitrateItem item)
+{
+    (void)ch;
+    (void)item;
+    return;
+}
 
--- test_bitrate/src/main.c
+++ test_bitrate/src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main(int argc, char** argv)
+#include "bitrate_calc.h"
+
+int main(int argc, char* argv[])
 {
     printf("hello world!\n");
+    rc_check(0);
+    BitrateItem item ={0};
+    rc_filled(0, item);
+
     return 0;
 }
