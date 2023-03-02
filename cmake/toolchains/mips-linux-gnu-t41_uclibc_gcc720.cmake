9b6598c00b8ada7f39e283e72c1412010f26cde2
huguanghui <guanghui.hu@simshine.ai>
2023-03-02 17:20:11 +0800

    feat(sys_proc)

--- cmake/toolchains/mips-linux-gnu-t41_uclibc_gcc720.cmake
+++ cmake/toolchains/mips-linux-gnu-t41_uclibc_gcc720.cmake
@@ -3,15 +3,9 @@ set(CMAKE_CROSSCOMPILING TRUE)
 set(CMAKE_SYSTEM_PROCESSOR mips)
 
 # Specify the cross compiler.
-set(CMAKE_C_COMPILER
-    /opt/mips-gcc720-glibc229-r5.1.4/bin/mips-linux-gnu-gcc
-    CACHE FILEPATH "C compiler")
-set(CMAKE_CXX_COMPILER
-    /opt/mips-gcc720-glibc229-r5.1.4/bin/mips-linux-gnu-g++
-    CACHE FILEPATH "C++ compiler")
-set(CMAKE_ASM_COMPILER
-    /opt/mips-gcc720-glibc229-r5.1.4/bin/mips-linux-gnu-gcc
-    CACHE FILEPATH "ASM compiler")
+set(CMAKE_C_COMPILER /opt/mips-gcc720-glibc229-r5.1.4/bin/mips-linux-gnu-gcc CACHE FILEPATH "C compiler")
+set(CMAKE_CXX_COMPILER /opt/mips-gcc720-glibc229-r5.1.4/bin/mips-linux-gnu-g++ CACHE FILEPATH "C++ compiler")
+set(CMAKE_ASM_COMPILER /opt/mips-gcc720-glibc229-r5.1.4/bin/mips-linux-gnu-gcc CACHE FILEPATH "ASM compiler")
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -muclibc -march=mips32r2")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -muclibc -march=mips32r2")
---
+++ sys_proc/sys_proc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    printf("[HGH-TEST][%s %d]\n", __FUNCTION__, __LINE__);
+
+    return EXIT_SUCCESS;
+}
